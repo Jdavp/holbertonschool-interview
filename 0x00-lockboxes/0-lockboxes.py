4df599bbdfac2af21dd8c67dc28686af02a0c679
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-20 05:27:02 +0000

    [fix] delete prints

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,7 +5,6 @@
 def canUnlockAll(boxes):
     "determines if all the boxes can be opened"
     all_boxes = []
-    print(len(boxes))
     for i in range(len(boxes)):
         if i not in boxes[i]:
             if len(boxes[i]) == 0:
@@ -13,8 +12,6 @@ def canUnlockAll(boxes):
             else:
                 all_boxes.append(boxes[i])
     set_of_boxes = set([item for box in all_boxes for item in box])
-    print(len(set_of_boxes))
-    print(len(boxes))
     if len(set_of_boxes) == len(boxes):
         return True
     else:
