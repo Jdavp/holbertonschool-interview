97a45943afc2b3725c43cc283cde2fe62b71aee3
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-21 23:50:02 +0000

    [fix]change logic

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,15 +4,14 @@
 
 def canUnlockAll(boxes):
     "determines if all the boxes can be opened"
-    all_boxes = []
-    for i in range(len(boxes)):
-        if i not in boxes[i]:
-            if len(boxes[i]) == 0:
-                all_boxes.append([0])
-            else:
-                all_boxes.append(boxes[i])
-    set_of_keys = set([keys for box in all_boxes
-                       for keys in box])
+    all_keys = []
+    for box in range(len(boxes)):
+        if len(boxes[box]) == 0:
+            all_keys.append(0)
+        for keys in boxes[box]:
+            if keys != box and keys <= len(boxes):
+                all_keys.append(keys)
+    set_of_keys = set(all_keys)
     if len(set_of_keys) == len(boxes):
         return True
     else:
