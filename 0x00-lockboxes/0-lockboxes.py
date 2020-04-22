6c33aebba009f0f512b8bd3be8f86838dfbcb1bd
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-04-22 02:26:46 +0000

    [fix]refactor

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -4,13 +4,14 @@
 
 def canUnlockAll(boxes):
     "determines if all the boxes can be opened"
-    all_keys = [0]
+    all_keys = []
     for box in range(len(boxes)):
         for keys in range(len(boxes[box])):
-            if boxes[box][keys] != box and boxes[box][keys] <= len(boxes):
+            if boxes[box][keys] != box and boxes[box][keys] < len(boxes) \
+               and boxes[box][keys] != 0:
                 all_keys.append(boxes[box][keys])
     set_of_keys = set(all_keys)
-    if len(set_of_keys) == len(boxes):
+    if len(set_of_keys) == (len(boxes)-1):
         return True
     else:
         return False
