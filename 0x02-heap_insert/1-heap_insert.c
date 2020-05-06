4e2e2edfd8bea87053516f049519c9f1d7993e6d
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-06 04:59:19 +0000

    [fix]change logic

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -13,30 +13,42 @@ heap_t *heap_insert(heap_t **root, int value)
 {
 	heap_t *node;
 
-	if ((*root) == NULL)
-	{
-		return (NULL);
-	}
 	node = binary_tree_node(*root, value);
+	if (*root == NULL)
+	{
+		*root = node;
+		return (node);
+	}
 	if ((*root)->right == NULL)
 	{
 		(*root)->right = node;
-	}
-	else if ((*root)->right != NULL)
-	{
-		node->right = (*root)->right;
-		(*root)->right->parent = node;
-		(*root)->right = node;
+		return (node);
 	}
 	if ((*root)->left == NULL)
 	{
 		(*root)->left = node;
+		return (node);
 	}
-	else
+	if (node->n > (*root)->n)
 	{
-		node->left = (*root)->left;
-		(*root)->left->parent = node;
-		(*root)->left = node;
+		int temp = 0;
+
+		if ((*root)->right != NULL)
+		{
+			temp = (*root)->right->n;
+			(*root)->right->n = node->n;
+			node->n = temp;
+			return (node);
+		}
+		if ((*root)->left != NULL)
+		{
+			temp = (*root)->left->n;
+			(*root)->left->n = node->n;
+			node->n = temp;
+			return (node);
+		}
 	}
+	heap_insert(&node->right, value);
+	heap_insert(&node->left, value);
 	return (node);
 }
