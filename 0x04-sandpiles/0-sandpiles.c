3f4e649508c59a800f7628ffd9846462ab17cc32
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-20 05:58:28 +0000

    [add]print and betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -1,8 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
-
 #include "sandpiles.h"
 
+
+/**
+ * sandpiles_sum - add sand matrices
+ *@grid1: first greed
+ *@grid2: second greed
+ * Return: Always 0 (Success)
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int a = 0;
@@ -21,29 +27,59 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		if (a == 1)
 		{
 			recursion(grid1);
+			print_grid(grid1);
+
 		}
 }
 
-void recursion(int grid1[3][3])
+/**
+ * recursion - reorganize sand
+ *@grid: - first grid
+ * Return: Always 0 (Success)
+ */
+
+void recursion(int grid[3][3])
 {
 	for (int x = 0; x <= 3; x++)
 	{
 		for (int y = 0; y <= 3; y++)
 		{
-			int num = grid1[x][y];
+			int num = grid[x][y];
 
 			if (num >= 4)
 			{
-				grid1[x][y] -= 4;
+				grid[x][y] -= 4;
 				if (x + 1 <= 3)
-					grid1[x + 1][y]++;
+					grid[x + 1][y]++;
 				if (x - 1 >= 0)
-					grid1[x - 1][y]++;
+					grid[x - 1][y]++;
 				if (y + 1 <= 3)
-					grid1[x][y + 1]++;
+					grid[x][y + 1]++;
 				if (y - 1 >= 0)
-					grid1[x][y - 1]++;
+					grid[x][y - 1]++;
 			}
 		}
+	}
+}
+
+/**
+ * print_grid - creates a binary tree node
+ *@grid: grid to be print
+ * Return: Always 0 (Success)
+ */
+
+static void print_grid(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (j)
+				printf(" ");
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
 	}
 }
