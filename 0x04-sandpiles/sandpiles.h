fcbd313ecd3fe84f321ef2f725272a415cc6e7bb
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-20 05:00:25 +0000

    [new] first attempt 0

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,14 @@
+#ifndef SANDPILES_H
+#define SANDPILES_H
+
+/**
+ * sanpiles_sum - sand piles sum
+ * @grid1: integer
+ * @grid2: stable structures
+ *
+ * Description: function that computes the sum of two sandpiles
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void recursion(int grid1[3][3]);
+
+#endif /* SANDPILES_H */
