30677fe94ad822888a69e6f3ba9772e5e40f8564
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-05-20 05:39:16 +0000

    [fix] after betty

--- 0x04-sandpiles/0-sandpiles.c
+++ 0x04-sandpiles/0-sandpiles.c
@@ -6,29 +6,43 @@
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int a = 0;
-    for (int x = 0; x <= 3; x++) {
-        for (int y = 0; y <= 3; y++) {
-		grid1[x][y] += grid2[x][y];
-		if (grid1[x][y] <= 4){
-	        a = 1;
+
+	for (int x = 0; x <= 3; x++)
+	{
+		for (int y = 0; y <= 3; y++)
+		{
+			grid1[x][y] += grid2[x][y];
+			if (grid1[x][y] <= 4)
+			{
+				a = 1;
+			}
 		}
-        }
-    }
-    if (a == 1){
-	    recursion(grid1);
-    }
+	}
+		if (a == 1)
+		{
+			recursion(grid1);
+		}
 }
 
-void recursion(int grid1[3][3]) {
-	for (int x = 0; x <= 3; x++) {
-		for (int y = 0; y <= 3; y++) {
+void recursion(int grid1[3][3])
+{
+	for (int x = 0; x <= 3; x++)
+	{
+		for (int y = 0; y <= 3; y++)
+		{
 			int num = grid1[x][y];
-			if (num >= 4) {
+
+			if (num >= 4)
+			{
 				grid1[x][y] -= 4;
-				if (x + 1 <=3) grid1[x + 1][y]++;
-				if (x - 1 >= 0) grid1[x - 1][y]++;
-				if (y + 1 <= 3) grid1[x][y + 1]++;
-				if (y - 1 >= 0) grid1[x][y - 1]++;
+				if (x + 1 <= 3)
+					grid1[x + 1][y]++;
+				if (x - 1 >= 0)
+					grid1[x - 1][y]++;
+				if (y + 1 <= 3)
+					grid1[x][y + 1]++;
+				if (y - 1 >= 0)
+					grid1[x][y - 1]++;
 			}
 		}
 	}
