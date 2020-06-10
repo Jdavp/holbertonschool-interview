51a77fb2c8a7a72e53c9836188ee2ab467d35d93
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-06-10 02:06:14 +0000

    [new] first attempt 0

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to head of list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *tortoise;
+	listint_t *hear;
+
+	tortoise = list;
+	hear = list->next;
+	while (tortoise->next != NULL && hear->next != NULL)
+	{
+		if (hear == tortoise)
+		{
+			return (1);
+		}
+		tortoise = tortoise->next;
+		hear = hear->next->next;
+	}
+	return (0);
+}
