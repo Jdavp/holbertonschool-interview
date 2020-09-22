fe2a3de8bae6c55f48edc3cefa3e77c117bb814a
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-09-22 15:21:48 -0500

    [new] UTF-8 Validation

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'UTF-8 Validation'
+import itertools as it
+
+def to_bits(bytes):
+    'Changing a number to a list of bits'
+    NUMBER_OF_BITS_PER_BLOCK = 8
+    
+    for byte in bytes:
+        num = []
+        exp = 1 << NUMBER_OF_BITS_PER_BLOCK
+        while exp:
+            exp >>= 1
+            num.append(bool(byte & exp))
+        yield num
+
+def validUTF8(data):
+    'determines if a given data set represents a valid UTF-8 encoding'
+    """
+    :type data: List[int]
+    :rtype: bool
+    """
+
+    MAX_NUMBER_OF_ONES = 4
+    
+    bits = to_bits(data)
+    for byte in bits:
+        # single byte char
+        if byte[0] == 0:
+            continue
+
+        # multi-byte character
+        amount = sum(it.takewhile(bool, byte))
+        if amount <= 1:
+            return False
+        if amount >= MAX_NUMBER_OF_ONES:
+            return False
+
+        for _ in range(amount - 1):
+            try:
+                byte = next(bits)
+            except StopIteration:
+                return False
+            if byte[0:2] != [1, 0]:
+                return False
+    return True
