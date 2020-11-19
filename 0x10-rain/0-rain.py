aa84331986adb4994bff9d5cc5a7a34232eab23d
Juan Diego Alejandro Valencia Peña <juandiegoalejandro@hotmail.com>
2020-11-19 16:03:06 -0500

    [new] first commit

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'Rain'
+
+
+def rain(walls: list) -> int:
+    'Return Integer indicating total amount of rainwater retained'
+    if (len(walls) > 0):
+        return sum(walls[1:-1])
+    return 0
